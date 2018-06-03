2cdbc4dee877cf7763ba320fb25f4c8bcc985ce0
Adam Fidel <refuse@gmail.com>
2018-06-03 13:59:03 -0500

    Add documentation.

--- src/lib/earnings.js
+++ src/lib/earnings.js
@@ -1,5 +1,9 @@
 import numeral from "numeral";
 
+/**
+ * Compute the partial sum of an array of numbers
+ * @param {Number[]} arr The array of numbers
+ */
 export function partialSum(arr) {
   return arr.reduce((r, a) => {
     r.push(((r.length && r[r.length - 1]) || 0) + a);
@@ -7,6 +11,12 @@ export function partialSum(arr) {
   }, []);
 }
 
+/**
+ * Compute an array of salary for every year
+ * @param {Number} years How many years to include
+ * @param {Number} starting The employee's starting salary
+ * @param {Number} raise Integer percentage of raise each year
+ */
 export function salary(years, starting, raise) {
   let arr = Array(years).fill(starting);
 
@@ -18,6 +28,11 @@ export function salary(years, starting, raise) {
   return arr;
 }
 
+/**
+ * String to display how much a student lost/earned by going for a PhD
+ * @param {Number[]} phd Salary for PhD student every year
+ * @param {Number[]} swe Salary for software engineer every year
+ */
 export function lostString(phd, swe) {
   const years = phd.length;
   const lostIncome = partialSum(swe)[years - 1] - partialSum(phd)[years - 1];
